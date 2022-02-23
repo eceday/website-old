0921536aad830f032598cd28c02a46316235a19f
Clyde Baron Rapinan <clydebaron2000@gmail.com>
2022-02-22 16:11:26 -0800

    accordion minipush (not finished)

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -1,9 +1,11 @@
+import styles from './accordion.module.css';
 function Accordian(props) {
-    
+    // TODO: add aria-labelledby
+    // TODO: fix classes
     return (
-        <div className="Accordion-shell">
-            <button className="Accordion-title">{props.title}</button>
-            <div>
+        <div className={styles.shell}>
+            <button className={styles.title}>{props.title}</button>
+            <div className={styles.content}>
                 <p>{props.content}</p>
             </div>
         </div>
