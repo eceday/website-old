9240db4c2ee41d421426094a0ef4f2b6197e1f10
clydebaronrapinan <clydebaron2000@gmail.com>
2022-03-02 13:43:08 -0800

    finished accordian

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -1,15 +1,34 @@
 import styles from './accordion.module.css';
-function Accordian(props) {
-    // TODO: add aria-labelledby
-    // TODO: fix classes
+import { motion, AnimatePresence } from "framer-motion";
+import {useState} from 'react';
+function Accordion(props) {
+    const [active, setExpanded] = useState(false);
+    let AccordionHandler = () => {
+        setExpanded(!active);
+    }
     return (
-        <div className={styles.shell}>
-            <button className={styles.title}>{props.title}</button>
-            <div className={styles.content}>
-                <p>{props.content}</p>
-            </div>
-        </div>
+        <motion.div className={styles.shell}>
+            <motion.button className={styles.title} onClick={AccordionHandler}>
+                {props.title}
+                <motion.div className={styles.plus} whileHover={{ scale: 1.1 }}>{(active)?"+":"-"}</motion.div>
+            </motion.button>
+            <AnimatePresence initial={false}>
+                {active && <motion.section 
+                    key="content"
+                    initial="collapsed"
+                    animate="open"
+                    exit="collapsed"
+                    variants={{
+                        open: { opacity: 1, height: "auto" ,padding:'10px'},
+                        collapsed: { opacity: 0, height: 0, padding: 0 }
+                    }}
+                    transition={{ duration: 0.8, ease: [0.00, 0.62, 0.23, 1] }}
+                    className={styles.content}>
+                    {props.content}
+                </motion.section>}
+            </AnimatePresence>
+        </motion.div>
     )
 }
 
-export default Accordian;
+export default Accordion;
